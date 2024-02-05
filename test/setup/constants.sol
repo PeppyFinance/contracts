9d9b71077e787c37d4e877bb30d206ae34423f7f
valleXYZ <valentin.seehausen@gmail.com>
2024-02-05 19:06:26 +0100

    add nice constants

--- test/setup/constants.sol
+++ test/setup/constants.sol
@@ -9,3 +9,14 @@ uint256 constant BOB_PK = 0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9
 address constant CATE = 0x90F79bf6EB2c4f870365E785982E1f101E93b906; // (3)
 uint256 constant CATE_PK = 0x7c852118294e51e653712a81e05800f419141751be58f605c371e15141b007a6;
 address constant DAN = 0x15d34AAf54267DB7D7c367839AAf71A00a2C6A65;
+
+int8 constant LONG = 1;
+int8 constant SHORT = -1;
+
+// leverages
+
+uint256 constant _1X = 1_000_000;
+uint256 constant _2X = 2_000_000;
+uint256 constant _3X = 3_000_000;
+uint256 constant _4X = 4_000_000;
+uint256 constant _5X = 5_000_000;
