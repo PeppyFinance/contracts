1710db27c10f52b915f24a5c8c83aa6f73a788d4
valleXYZ <valentin.seehausen@gmail.com>
2024-02-12 16:49:33 +0100

    add test_pnl_loss_long

--- test/PositionPnL.t.sol
+++ test/PositionPnL.t.sol
@@ -38,4 +38,16 @@ contract PositionPnLTest is Test, WithHelpers {
 
         assertEq(collateralToken.balanceOf(BOB), 200 ether, "should have made 100% profit");
     }
+
+    function test_pnl_loss_long() public {
+        _deposit(ALICE, 1000 ether);
+        _setPrice(address(collateralToken), 1000 ether);
+
+        _openPosition(BOB, 100 ether, LONG, _5X);
+
+        _setPrice(address(collateralToken), 900 ether);
+        _closePosition(BOB, 1);
+
+        assertEq(collateralToken.balanceOf(BOB), 50 ether, "should have lost 50%");
+    }
 }
