4a5e32e74bf06279c2e2cfb0f3b6940683cab7e1
valleXYZ <valentin.seehausen@gmail.com>
2024-02-12 17:51:30 +0100

    add test_pnl_withFees_fundingRebate

--- test/PositionPnL.t.sol
+++ test/PositionPnL.t.sol
@@ -81,8 +81,8 @@ contract PositionPnLTest is Test, WithHelpers {
 
         _openPosition(BOB, 100 ether, LONG, _5X);
 
-        assertEq(_liquidityPool_getBorrowRate(), 3 * BPS, "borrow rate should be 3%");
-        assertEq(_tradePair_getFundingRate(), 5 * BPS, "funding rate should be 5%");
+        assertEq(_liquidityPool_getBorrowRate(), 3 * BPS, "borrow rate should be 3 BPS");
+        assertEq(_tradePair_getFundingRate(), 5 * BPS, "funding rate should be 5 BPS");
 
         vm.warp(2 hours);
 
@@ -116,8 +116,8 @@ contract PositionPnLTest is Test, WithHelpers {
 
         _openPosition(BOB, 100 ether, SHORT, _5X);
 
-        assertEq(_liquidityPool_getBorrowRate(), 3 * BPS, "borrow rate should be 3%");
-        assertEq(_tradePair_getFundingRate(), -5 * BPS, "funding rate should be 5%");
+        assertEq(_liquidityPool_getBorrowRate(), 3 * BPS, "borrow rate should be 3 BPS");
+        assertEq(_tradePair_getFundingRate(), -5 * BPS, "funding rate should be 5 BPS");
 
         vm.warp(2 hours);
 
@@ -151,8 +151,8 @@ contract PositionPnLTest is Test, WithHelpers {
 
         _openPosition(BOB, 100 ether, LONG, _5X);
 
-        assertEq(_liquidityPool_getBorrowRate(), 3 * BPS, "borrow rate should be 3%");
-        assertEq(_tradePair_getFundingRate(), 5 * BPS, "funding rate should be 5%");
+        assertEq(_liquidityPool_getBorrowRate(), 3 * BPS, "borrow rate should be 3 BPS");
+        assertEq(_tradePair_getFundingRate(), 5 * BPS, "funding rate should be 5 BPS");
 
         vm.warp(2 hours);
 
@@ -184,8 +184,8 @@ contract PositionPnLTest is Test, WithHelpers {
 
         _openPosition(BOB, 100 ether, SHORT, _5X);
 
-        assertEq(_liquidityPool_getBorrowRate(), 3 * BPS, "borrow rate should be 3%");
-        assertEq(_tradePair_getFundingRate(), -5 * BPS, "funding rate should be 5%");
+        assertEq(_liquidityPool_getBorrowRate(), 3 * BPS, "borrow rate should be 3 BPS");
+        assertEq(_tradePair_getFundingRate(), -5 * BPS, "funding rate should be 5 BPS");
 
         vm.warp(2 hours);
 
@@ -200,6 +200,44 @@ contract PositionPnLTest is Test, WithHelpers {
         assertEq(collateralToken.balanceOf(BOB), payOut, "should be payout");
         assertEq(
             collateralToken.balanceOf(address(liquidityPool)), 1000 ether + 100 ether - payOut, "LP should receive loss"
+        );
+    }
+
+    function test_pnl_withFees_fundingRebate() public {
+        _tradePair_setOpenFee(10 * BPS);
+        _tradePair_setCloseFee(10 * BPS);
+        _liquidityPool_setMinBorrowRate(1 * BPS);
+        _liquidityPool_setMaxBorrowRate(5 * BPS);
+        _tradePair_setMaxFundingRate(5 * BPS);
+
+        _deposit(ALICE, 998.5 ether); // will receive 1.5 from open fee
+        _setPrice(address(collateralToken), 1000 ether);
+
+        vm.warp(1 hours);
+
+        _openPosition(BOB, 100 ether, LONG, _5X);
+        _openPosition(ALICE, 200 ether, SHORT, _5X);
+
+        assertEq(collateralToken.balanceOf(address(liquidityPool)), 1000 ether, "LP balance should be 1000");
+        assertEq(tradePair.excessOpenInterest(), 500 ether, "excess open interest should be 500");
+        assertEq(_tradePair_getBorrowRate(), 3 * BPS, "borrow rate should be 3 BPS");
+        assertEq(_tradePair_getFundingRate(), -2 * BPS, "funding rate should be -2 BPS");
+
+        vm.warp(2 hours);
+
+        uint256 feeAmount = 5 * 100 ether * (3 - 2) / 10_000;
+        uint256 value = 200 ether - feeAmount;
+        uint256 closeFee = value * 10 / 10_000;
+        uint256 payOut = value - closeFee;
+
+        _setPrice(address(collateralToken), 1200 ether);
+        _closePosition(BOB, 1);
+
+        assertEq(collateralToken.balanceOf(BOB), payOut, "should be payout");
+        assertEq(
+            collateralToken.balanceOf(address(liquidityPool)),
+            1000 ether + 100 ether - payOut,
+            "LP should pay out 80% profit"
         );
     }
 }
