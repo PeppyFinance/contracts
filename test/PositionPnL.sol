d04a8f7099b89a3a71fc9e663378ec4e14802a41
valleXYZ <valentin.seehausen@gmail.com>
2024-02-12 12:07:31 +0100

    add PositionPnLTest

---
+++ test/PositionPnL.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.18;
+
+import "forge-std/Test.sol";
+import "src/TradePair.sol";
+import "src/LiquidityPool.sol";
+import "src/Controller.sol";
+import "test/setup/MockPriceFeed.sol";
+import "test/setup/constants.sol";
+import "test/setup/WithHelpers.sol";
+import "openzeppelin/token/ERC20/ERC20.sol";
+
+contract PositionPnLTest is Test, WithHelpers {
+    function setUp() public {
+        _deployTestSetup();
+    }
+}
