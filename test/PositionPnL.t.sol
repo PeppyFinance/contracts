1fdba1e3022d49e1e83bb23da925d28d73bc76e3
valleXYZ <valentin.seehausen@gmail.com>
2024-02-12 17:31:55 +0100

    add test_pnl_withFees_profit_short

--- test/PositionPnL.t.sol
+++ test/PositionPnL.t.sol
@@ -101,4 +101,39 @@ contract PositionPnLTest is Test, WithHelpers {
             "LP should pay out 80% profit"
         );
     }
+
+    function test_pnl_withFees_profit_short() public {
+        _tradePair_setOpenFee(10 * BPS);
+        _tradePair_setCloseFee(10 * BPS);
+        _liquidityPool_setMinBorrowRate(1 * BPS);
+        _liquidityPool_setMaxBorrowRate(5 * BPS);
+        _tradePair_setMaxFundingRate(5 * BPS);
+
+        _deposit(ALICE, 999.5 ether); // will receive 0.5 from open fee
+        _setPrice(address(collateralToken), 1000 ether);
+
+        vm.warp(1 hours);
+
+        _openPosition(BOB, 100 ether, SHORT, _5X);
+
+        assertEq(_liquidityPool_getBorrowRate(), 3 * BPS, "borrow rate should be 3%");
+        assertEq(_tradePair_getFundingRate(), -5 * BPS, "funding rate should be 5%");
+
+        vm.warp(2 hours);
+
+        uint256 feeAmount = 8 * 100 ether * 5 / 10_000;
+        uint256 value = 200 ether - feeAmount;
+        uint256 closeFee = value * 10 / 10_000;
+        uint256 payOut = value - closeFee;
+
+        _setPrice(address(collateralToken), 800 ether);
+        _closePosition(BOB, 1);
+
+        assertEq(collateralToken.balanceOf(BOB), payOut, "should be payout");
+        assertEq(
+            collateralToken.balanceOf(address(liquidityPool)),
+            1000 ether + 100 ether - payOut,
+            "LP should pay out 80% profit"
+        );
+    }
 }
