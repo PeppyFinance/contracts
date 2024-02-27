d6615d7d2ea61e4c2e8654096fd650465cc9b511
valleXYZ <valentin.seehausen@gmail.com>
2024-02-27 19:39:33 +0100

    remove deploy

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# To load the variables in the .env file
-source .env
-
-# To deploy and verify our contract
-forge script script/deploy.s.sol:Deploy --rpc-url $RPC_URL --broadcast --verify -vvvv --legacy --private-key $PRIVATE_KEY
