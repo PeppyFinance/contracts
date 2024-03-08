dfe3d6b1ebe7e79eb47cd91830387d23d3f6f3ec
valleXYZ <valentin.seehausen@gmail.com>
2024-03-08 21:45:41 +0100

    add stale price check

--- test/Deployment.t.sol
+++ test/Deployment.t.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.18;
 import "forge-std/Test.sol";
 import "test/setup/WithDeployment.t.sol";
 import "pyth-sdk-solidity/IPyth.sol";
-import "pyth-sdk-solidity/MockPyth.sol";
+import "pyth-sdk-solidity/PythErrors.sol";
 
 contract DeploymentTest is WithDeployment {
     function setUp() public {
@@ -28,7 +28,7 @@ contract DeploymentTest is WithDeployment {
         );
     }
 
-    function test_pythPriceFeed() public {
+    function test_pythPriceFeed_success() public {
         bytes32 id = bytes32("btc");
         IPyth pyth = IPyth(_getConstant("PYTH"));
         bytes memory updateData =
@@ -39,5 +39,17 @@ contract DeploymentTest is WithDeployment {
         PythStructs.Price memory price = pyth.getPriceNoOlderThan(id, 0);
         assertEq(price.price, 123, "Price should be 123");
         assertEq(price.conf, 456, "Price should be 456");
+    }
+
+    function test_pythPriceFeed_stale() public {
+        vm.warp(10);
+        bytes32 id = bytes32("btc");
+        IPyth pyth = IPyth(_getConstant("PYTH"));
+        bytes memory updateData = MockPyth(address(pyth)).createPriceFeedUpdateData(id, 123, 456, 789, 120, 400, 8);
+        bytes[] memory updateDataArray = new bytes[](1);
+        updateDataArray[0] = updateData;
+        pyth.updatePriceFeeds{value: 1}(updateDataArray);
+        vm.expectRevert(abi.encodeWithSelector(PythErrors.StalePrice.selector));
+        pyth.getPriceNoOlderThan(id, 0);
     }
 }
