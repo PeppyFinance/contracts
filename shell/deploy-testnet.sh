1d9e1bb858ed351ab2cb6fa2680534174da1a49d
valleXYZ <valentin.seehausen@gmail.com>
2024-03-19 18:10:41 +0100

    deploy testnet

--- shell/deploy-testnet.sh
+++ shell/deploy-testnet.sh
@@ -4,4 +4,4 @@
 source .env
 
 # To deploy and verify our contract
-NETWORK=testnet forge script script/testnet/DeployRealPyth.s.sol --fork-url https://json-rpc.evm.testnet.shimmer.network -vvvv --legacy
+NETWORK=testnet forge script script/testnet/DeployRealPyth.s.sol --fork-url https://json-rpc.evm.testnet.shimmer.network -vvvv --legacy --broadcast
